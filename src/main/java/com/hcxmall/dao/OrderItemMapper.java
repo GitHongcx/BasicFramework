c4c5311f69db7c7d12590f75903ed0eb92c0821d
HCX <948957717@qq.com>
2017-11-03 19:06:17 +0800

    基本框架

---
+++ src/main/java/com/hcxmall/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.hcxmall.dao;
+
+import com.hcxmall.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/hcxmall/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.hcxmall.dao;
+
+import com.hcxmall.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
